e05646c669e76ce63f7100e01afe886dc08d3740
NickyFasoel <nickyfasoel@telenet.be>
2016-01-22 19:17:58 +0100

    V 33
    added jacoco en teste eens

--- Code/ProjectDesktop - in werking/build/generated-sources/ap-source-output/Dal/Film_.java
+++ Code/ProjectDesktop - in werking/build/generated-sources/ap-source-output/Dal/Film_.java
@@ -4,11 +4,11 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-01-13T09:09:26")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-01-22T19:14:23")
 @StaticMetamodel(Film.class)
 public class Film_ { 
 
-    public static volatile SingularAttribute<Film, String> foto;
+    public static volatile SingularAttribute<Film, String> image;
     public static volatile SingularAttribute<Film, String> speelDagen;
     public static volatile SingularAttribute<Film, Integer> imgCorr;
     public static volatile SingularAttribute<Film, String> genre;
--- Code/ProjectDesktop - in werking/build/generated-sources/ap-source-output/Dal/Klant_.java
+++ Code/ProjectDesktop - in werking/build/generated-sources/ap-source-output/Dal/Klant_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-01-13T09:09:26")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-01-22T19:14:23")
 @StaticMetamodel(Klant.class)
 public class Klant_ { 
 
